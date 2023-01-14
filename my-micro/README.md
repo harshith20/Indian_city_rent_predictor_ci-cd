211b2be5b7ce860cc0347c621413e00c413bae2d
Areti harshith swamy <harshithareti@gmail.com>
2023-01-14 22:45:25 +0530

    d

--- my-micro/README.md
+++
@@ -1,2 +0,0 @@
-# Indian_city_rent_predictor_ci-cd
-predicts rent for flats in different cities based on amenities provided
--- my-micro/main.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)  # notice that the app instance is called `app`, this is very important.
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
