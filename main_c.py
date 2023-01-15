b490869bb5a8f70d62cb2feed3b9d6d9e13f8623
Areti harshith swamy <harshithareti@gmail.com>
2023-01-15 19:53:33 +0530

    doc

--- main_c.py
+++ main_c.py
@@ -1,13 +1,13 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import os
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-import json
-from sklearn.model_selection import KFold
-from sklearn.model_selection import cross_val_score
-from sklearn.ensemble import RandomForestRegressor
-from numpy import mean
-from numpy import std
-import pickle
+# import numpy as np # linear algebra
+# import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+# import os
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# from sklearn.model_selection import train_test_split
+# import json
+# from sklearn.model_selection import KFold
+# from sklearn.model_selection import cross_val_score
+# from sklearn.ensemble import RandomForestRegressor
+# from numpy import mean
+# from numpy import std
+# import pickle
